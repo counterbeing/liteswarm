132451a2762f036519b9c7e0698ea5b86eac1fc9
Cory Logan <he@corylogan.com>
2016-08-07 18:36:16 -0700

    adjust animation speeds

--- strandtest.ino
+++ strandtest.ino
@@ -1,10 +1,3 @@
-// Simple strand test for Adafruit Dot Star RGB LED strip.
-// This is a basic diagnostic tool, NOT a graphics demo...helps confirm
-// correct wiring and tests each pixel's ability to display red, green
-// and blue and to forward data down the line.  By limiting the number
-// and color of LEDs, it's reasonably safe to power a couple meters off
-// the Arduino's 5V pin.  DON'T try that with other code!
-
 #include <Adafruit_DotStar.h>
 #include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
 #include <Bounce2.h>
@@ -18,14 +11,6 @@
 
 Adafruit_DotStar strip = Adafruit_DotStar(
   NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
-// The last parameter is optional -- this is the color data order of the
-// DotStar strip, which has changed over time in different production runs.
-// Your code just uses R,G,B colors, the library then reassigns as needed.
-// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
-
-// Hardware SPI is a little faster, but must be wired to specific pins
-// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
-//Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BRG);
 
 int buttonPin =  A0;
 int rotary1 = 2;
@@ -109,28 +94,31 @@ void strand_off(){
 }
 
 void race(){
+ int dlay = confineRotary(5, 500);
  complete_color(off); 
  head++;
  if(head > NUMPIXELS) head = 0;
  strip.setPixelColor(head, blue);
  strip.show();
- delay(10);
+ delay(dlay);
 }
 
 void dazzle(){
+  int dlay = confineRotary(5, 500);
   complete_color(red);
-  delay(10);
+  delay(dlay);
   complete_color(green);
-  delay(10);
+  delay(dlay);
   complete_color(blue);
-  delay(10);
+  delay(dlay);
 }
 
 void strobe(){
+   int dlay = confineRotary(5, 500);
   complete_color(0xFFFFFF);
-  delay(50);
+  delay(dlay);
   complete_color(off);
-  delay(50);
+  delay(dlay);
 }
 
 void complete_color(uint32_t color){
@@ -138,4 +126,16 @@ void complete_color(uint32_t color){
     strip.setPixelColor(leds, color);
   }
   strip.show();
+}
+
+int confineRotary(int start, int finish) {
+  if(posRotary < start) { 
+    posRotary = start;
+    knob.write(start);
+  }
+  if(posRotary > finish) {
+    posRotary = finish;
+    knob.write(finish);
+  }
+  return posRotary;
 }
