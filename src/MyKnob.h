3a13cebbfc722f2d2babe78533403996e1c324b8
Cory Logan <he@corylogan.com>
2019-06-07 02:07:15 -0400

    problematic

--- src/MyKnob.h
+++ src/MyKnob.h
@@ -8,13 +8,53 @@ Bounce button_debouncer = Bounce();
 
 class MyKnob
 {
-public:
+
+private:
+    // class knob;
+    // int knob;
+    Encoder knob(2,3);
+    int pinA;
+    int pinB;
     int position;
-    MyKnob(int a, int b)
+    int start;
+    int finish;
+    int buttonPressCount = 0;
+    long lastPressTime = 0;
+    void checkRotary(int loopTime)
+    {
+        long newPos = knob.read();
+        if (newPos != position)
+        {
+            position = newPos;
+            /* Serial.println(newPos); */
+        }
+    }
+
+    void checkButton(int &animationIndex)
+    {
+        int buttonState = button_debouncer.read();
+        long currentPressTime = millis();
+        long difference = currentPressTime - lastPressTime;
+        if ((buttonState == 0) && (difference > 400))
+        {
+            buttonPressCount++;
+            /* Serial.print("Button press count: "); */
+            /* Serial.println(buttonPressCount); */
+            /* Serial.println("BUTTON PRESSED CALLING SWITCH"); */
+            /* Serial.println(lastPressTime); */
+            /* Serial.println(currentPressTime); */
+            lastPressTime = millis();
+            animationIndex++;
+        }
+    }
+
+public:
+    MyKnob(uint8_t a, uint8_t b)
     {
         pinA = a;
         pinB = b;
         // Encoder knob(pinA, pinB);
+        Encoder knob(2,3);
     }
     void check(unsigned long m, int &animationIndex) {
         checkButton(animationIndex);
@@ -41,41 +81,7 @@ public:
 
 
 
-private:
-    int pinA;
-    int pinB;
-    int position;
-    int start;
-    int finish;
-    int buttonPressCount = 0;
-    long lastPressTime = 0;
-    void checkRotary(int loopTime)
-    {
-        long newPos = knob.read();
-        if (newPos != position)
-        {
-            position = newPos;
-            /* Serial.println(newPos); */
-        }
-    }
-
-    int checkButton(int &animationIndex)
-    {
-        int buttonState = button_debouncer.read();
-        long currentPressTime = millis();
-        long difference = currentPressTime - lastPressTime;
-        if ((buttonState == 0) && (difference > 400))
-        {
-            buttonPressCount++;
-            /* Serial.print("Button press count: "); */
-            /* Serial.println(buttonPressCount); */
-            /* Serial.println("BUTTON PRESSED CALLING SWITCH"); */
-            /* Serial.println(lastPressTime); */
-            /* Serial.println(currentPressTime); */
-            lastPressTime = millis();
-            animationIndex++;
-        }
-    }
 };
 
 #endif
+
