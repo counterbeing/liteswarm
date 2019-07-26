5d82102affd1f8b1dc5472929deee57107723aa8
Cory Logan <he@corylogan.com>
2019-07-26 11:17:53 -0700

    with rainbow animation

---
+++ src/animations/Rainbow.h
@@ -0,0 +1,32 @@
+#include "Animation.h"
+
+class Rainbow : public Animation
+{
+private:
+    int initialPosition = 1;
+    int start = 1;
+    int finish = 255;
+    bool initialized = false;
+    bool loopRotary = true;
+    MyKnob &knob;
+    CRGB *leds;
+
+    void setup()
+    {
+        knob.setDefaults(
+            initialPosition,
+            start,
+            finish,
+            loopRotary);
+    };
+
+    void loop()
+    {
+        int offset = knob.confine();
+        fill_rainbow(leds, NUMPIXELS, offset, 1);
+        FastLED.show();
+    }
+
+public:
+    Rainbow(MyKnob &knob_, CRGB leds_[]) : knob(knob_), leds(leds_) {}
+};
