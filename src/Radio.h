7830283fb31bb15cfcc9c44a58fbafcc6735cdf1
Mac Cowell <mac@has100ideas.com>
2019-08-13 00:53:58 -0700

    configs for mac protoboard + breadboard

--- src/Radio.h
+++ src/Radio.h
@@ -21,8 +21,10 @@ private:
     MyKnob &knob;
     int &animation_index;
     const static uint8_t SHARED_RADIO_ID = 1;
-    const static uint8_t PIN_RADIO_CE = 6; // will be 6 on PCBs, was 9
-    const static uint8_t PIN_RADIO_CSN = 7; // will be 7 on PCBs, was 10
+    // const static uint8_t PIN_RADIO_CE = 6; // will be 6 on PCBs, was 9
+    // const static uint8_t PIN_RADIO_CSN = 7; // will be 7 on PCBs, was 10
+    const static uint8_t PIN_RADIO_CE = 9; // mac protoboard
+    const static uint8_t PIN_RADIO_CSN = 10; // mac protoboard
     const static uint8_t SHARED_SECRET = 42;
     uint8_t RADIO_ID = random();
     int previousAnimationIndex;
--- src/config.h
+++ src/config.h
@@ -2,11 +2,20 @@
 #define CONFIG_H
 
 // All that matters to a 3pin WS* strand
-#define DATAPIN 4
+// #define DATAPIN 4 // default for SCARFYMCSARFACE pcb
+#define DATAPIN 5 // mac's protoboard
 // Not applicable to 4 wire APA102 type strands
-#define CLOCKPIN 5
+// #define CLOCKPIN 5
+#define CLOCKPIN 7 // mac protoboard
 
 #define STRIP WS2811
 const int NUMPIXELS = 75;
+
+// knob defaults
+// buttonPin = A0
+// rotary1 = 2
+// rotary2 = 3
+// PIN_RADIO_CE = 6; // will be 6 on PCBs, was 9
+// PIN_RADIO_CSN = 7; // will be 7 on PCBs, was 10
 
 #endif
