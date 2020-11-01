5c25fea49caf8879ef57bc7e20aa39fcaa45664d
Cory Logan <he@corylogan.com>
2020-11-01 13:09:34 -0700

    adjust clang format

--- src/config.h
+++ src/config.h
@@ -1,20 +1,27 @@
 #ifndef CONFIG_H
-#define CONFIG_H
+#  define CONFIG_H
 
-#define KNOB_DEBUG false  // set flag to false to disable serial logging in knob
-#define RADIO_DEBUG false // set flag to false to disable logging for Radio
-#define ANIM_DEBUG false
+#  define KNOB_DEBUG                                                           \
+    false // set flag to false to disable serial logging in knob
+#  define RADIO_DEBUG false // set flag to false to disable logging for Radio
+#  define ANIM_DEBUG false
+
+#  if BOARD == "nano"
+#  elif BOARD == "esp32"
+#  else
+#    error "Unexpected value of BOARD."
+#  endif
 
 // #define SCARF_DOTSTAR
 // #define SCARF_WS2811     // IC has pink dot
-#define SCARF_SK9822 // IC has green dot
+#  define SCARF_SK9822 // IC has green dot
 // #define BIGRED_WS2815    // IC has purple dot?
 
 ////////////////////////////////////////////////////
 // DEFAULTS
 // All that matters to a 3pin WS* strand
-#define DATAPIN 5  // default for SCARFYMCSARFACE pcb
-#define CLOCKPIN 4 // Not applicable to 4 wire APA102 type strands
+#  define DATAPIN 5  // default for SCARFYMCSARFACE pcb
+#  define CLOCKPIN 4 // Not applicable to 4 wire APA102 type strands
 
 // const int NUMPIXELS = 75;
 //
@@ -27,21 +34,21 @@
 
 //////////////////////////////////////////////////
 // WS2811
-#ifdef SCARF_WS2811
+#  ifdef SCARF_WS2811
 const int NUMPIXELS = 75;
-#endif
+#  endif
 
 //////////////////////////////////////////////////
 // SK9822
-#ifdef SCARF_SK9822
+#  ifdef SCARF_SK9822
 const int NUMPIXELS = 75;
-#endif
+#  endif
 
 //////////////////////////////////////////////////
 // DOTSTAR
-#ifdef SCARF_DOTSTAR
+#  ifdef SCARF_DOTSTAR
 const int NUMPIXELS = 75;
-#endif
+#  endif
 
 //////////////////////////////////////////////////
 // BIGRED 12v WS2815 strip
@@ -51,9 +58,9 @@ const int NUMPIXELS = 75;
 //   controller green wire)
 //
 // #define CLOCKPIN 5
-#ifdef BIGRED_WS2815
+#  ifdef BIGRED_WS2815
 const int NUMPIXELS = 375;
-#endif
+#  endif
 
 //////////////////////////////////////////////////
 // mac's protoboard
