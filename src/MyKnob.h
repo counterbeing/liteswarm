37333fcdbe968bb14e1ce654ebd83c1b23bb3761
Cory Logan <he@corylogan.com>
2020-11-12 11:20:20 -0700

    fix conditional compilation

--- src/MyKnob.h
+++ src/MyKnob.h
@@ -6,6 +6,11 @@
 
 #ifndef MyKnob_H
 #  define MyKnob_H
-#  include "./MyKnob/MyKnobNano.h"
-// #  include "./MyKnob/MyKnobEsp32.h"
+#  if BOARD == BOARD_NANO
+#    include "./MyKnob/MyKnobNano.h"
+#  elif BOARD == BOARD_ESP32
+#    include "./MyKnob/MyKnobEsp32.h"
+#  else
+#    error "No `MyKnob` was defined for this `BOARD`"
+#  endif
 #endif
--- src/config.h
+++ src/config.h
@@ -10,7 +10,13 @@
 
 const int NUMPIXELS = 75;
 
-#if BOARD == nano
+// Here we define boards as integers to be used in `platform.ini` passed build
+// flags. This way we can use the easier to reference varaiable name to keep the
+// code readable when conditionally compiling blocks of code.
+#define BOARD_NANO 1
+#define BOARD_ESP32 2
+
+#if BOARD == BOARD_NANO
 const int CLOCKPIN = 4;
 const int DATAPIN = 5;
 const int buttonPin = A0;
@@ -18,7 +24,7 @@ uint8_t rotary1 = 2;
 uint8_t rotary2 = 3;
 // PIN_RADIO_CE = 6; // will be 6 on PCBs, was 9
 // PIN_RADIO_CSN = 7; // will be 7 on PCBs, was 10
-#elif BOARD == esp32
+#elif BOARD == BOARD_ESP32
 const int CLOCKPIN = 4;
 const int DATAPIN = 5;
 const int buttonPin = A0;
